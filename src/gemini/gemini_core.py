bbcc54cf74deadad6fee37c6dd2cb0952a1ac154
Mariia Shpir <99517424+shpirm@users.noreply.github.com>
2024-07-28 04:36:18 +0300

    init baseline

--- src/gemini/gemini_core.py
+++ src/gemini/gemini_core.py
@@ -2,15 +2,58 @@ import os
 
 import google.generativeai as genai
 
+import vertexai
+from vertexai.preview import reasoning_engines
+
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_google_vertexai import ChatVertexAI
+
+from typing import List, Union, Dict
 
 class GeminiCore:
     def __init__(self):
-        genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
-        self.model = genai.GenerativeModel("gemini-1.5-flash")
-        self.chat = self.model.start_chat(history=[])
+        
+        self.project_id = "neon-opus-429019-j1"
+        self.location = "us-central1"
+        self.staging_bucket = "gs://creativity-1"
 
-    def send_message(self, message):
-        return self.chat.send_message(message).text
+        vertexai.init(
+            project=self.project_id, 
+            location=self.location, 
+            staging_bucket=self.staging_bucket
+        )
+
+        # genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
+        # self.model = genai.GenerativeModel("gemini-1.5-flash")
+        # self.chat = self.model.start_chat(history=[])
+
+    def _set_up(self) -> None:
+        system = (
+            "You are a helpful assistant that answers questions "
+            "about Google Cloud."
+        )
+
+        human = "{text}"
+        prompt = ChatPromptTemplate.from_messages(
+            [("system", system), ("human", human)]
+        )
+        chat = ChatVertexAI(project=self.project_id, location=self.location)
+        self.chain = prompt | chat
+
+    def send_message(self, question: str) -> Union[str, List[Union[str, Dict]]]:
+        """Query the application.
+
+        Args:
+            question: The user prompt.
+
+        Returns:
+            str: The LLM response.
+        """
+        self._set_up()
+        return self.chain.invoke({"text": question}).content
+    
+    # def send_message(self, message):
+    #     return self.chat.send_message(message).text
 
     # def read_calendar(self, creds):
     #     return read_calendar_events(creds)
